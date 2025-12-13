f7521959c699a494f05e254d3f5f54879c26f901
Abd El-Rahman Hayder <lboodyde02@gmail.com>
2025-12-13 11:58:16 +0200

    resume

--- train_catt.py
+++ train_catt.py
@@ -121,7 +121,7 @@ trainer = Trainer(
     #accelerator="cpu",
     accelerator="cuda",
     devices=-1,
-    max_epochs=32,
+    max_epochs=48,
     callbacks=[TQDMProgressBar(refresh_rate=1), checkpoint_callback, early_stop],
     precision=16,
     logger=CSVLogger(save_dir=logs_path),
@@ -130,7 +130,7 @@ trainer = Trainer(
 
 #ckpt_path = 'YOUR_CKPT_PATH_GOES_HERE'
 #trainer.fit(model, train_dataloader, val_dataloader, ckpt_path=ckpt_path)
-ckpt_path = "/kaggle/working/catt/catt/catt_ed_model_v1_lora/last.ckpt"
+ckpt_path = "/kaggle/working/catt/catt_ed_model_v1_lora/last.ckpt"
 trainer.fit(
     model,
     train_dataloader,
